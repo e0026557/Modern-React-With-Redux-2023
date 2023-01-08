d7a3befcb51d16f0e52086b1e764de9b9b2fcd8f
Wesley Cheah <65939868+e0026557@users.noreply.github.com>
2023-01-08 02:37:39 +0000

    Added add album feature

--- media/src/store/apis/albumsApi.js
+++ media/src/store/apis/albumsApi.js
@@ -1,14 +1,31 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+
+// DEV ONLY!!!
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 const albumsApi = createApi({
   reducerPath: "albums",
   baseQuery: fetchBaseQuery({
     baseUrl:
       "https://e0026557-curly-meme-55vj6v4gx4rf4jj4-3005.preview.app.github.dev",
+    fetchFn: async (...args) => {
+      // REMOVE FOR PRODUCTION
+      await pause(1000);
+      return fetch(...args);
+    },
   }),
   endpoints(builder) {
     return {
       fetchAlbums: builder.query({
+        // arg is user in this case -> the arg passed into the useFetchAlbumsQuery hook
+        providesTags: (result, error, user) => {
+          return [{ type: "Album", id: user.id }];
+        },
         query: (user) => {
           return {
             url: "/albums",
@@ -19,9 +36,25 @@ const albumsApi = createApi({
           };
         },
       }),
+      addAlbum: builder.mutation({
+        // arg is user in this case -> arg from the function returned when we call useAddAlbumMutation hook
+        invalidatesTags: (result, error, user) => {
+          return [{ type: "Album", id: user.id }];
+        },
+        query: (user) => {
+          return {
+            url: "/albums",
+            method: "POST",
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
     };
   },
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
